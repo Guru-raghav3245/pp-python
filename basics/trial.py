b03f25b8d251502d6a71ed1a3358ca09393e1455
Guru Raghav <master.guru.raghav@gmail.com>
2022-11-30 15:45:12 +0530

    I finished it today

--- basics/trial.py
+++ basics/trial.py
@@ -8,3 +8,14 @@
 
 # The Coding part will be easy once you can figure out the math
 my_sequence = [-5, -4, -3, -2, -1, 0, 2]
+
+if my_sequence[0] - my_sequence[1] == my_sequence[2] - my_sequence[3]:
+    for t in range(4):
+        if (my_sequence[t]) - (my_sequence[t + 1]) != (my_sequence[t + 2]) - (my_sequence[t + 3]):
+            if (my_sequence[t] + 1) - (my_sequence[t + 1]) != (my_sequence[t + 2]) - (my_sequence[t + 3]):
+                if (my_sequence[t]) - (my_sequence[t] + 1) != (my_sequence[t + 2]) - (my_sequence[t + 3]):
+                    if (my_sequence[t] + 1) - (my_sequence[t + 1]) != (my_sequence[t + 2] + 1) - (my_sequence[t + 3]):
+                        if (my_sequence[t]) - (my_sequence[t + 1]) != (my_sequence[t + 2]) - (my_sequence[t + 3] + 1):
+                            print("I cant find it, sorry")
+                        else:
+                            print("    ")

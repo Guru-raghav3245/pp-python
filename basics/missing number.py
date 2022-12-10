d7b06568eb27a5e6c5dec354205cd2efd0d95389
Guru-raghav3245 <102360948+Guru-raghav3245@users.noreply.github.com>
2022-12-10 12:04:19 +0530

    updating changes

---
+++ basics/missing number.py
@@ -0,0 +1,18 @@
+series = [-1, -2, -4, -5]
+x = 0
+t = 0
+if series[-1] + 1 < 0:
+    a = -(series[-1] + 1)
+else:
+    a = series[-1] + 1
+for i in range(a):
+    x = x + i
+    i += 1
+    print(i)
+print(x)
+
+for y in range(len(series)):
+    t = t + series[y]
+print(t)
+
+print("The missing number is -", x - t)

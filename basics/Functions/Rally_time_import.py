59f33afa0dbad91004d66ea35dd6f660e7ffdb7d
Guru-raghav3245 <master.guru.raghav@gmail.com>
2023-01-13 12:42:46 +0530

    Rename

--- basics/Functions/Rally_time_import.py
+++ basics/Functions/Rally_time_import.py
@@ -1,4 +1,4 @@
-import Rally_time
+import Rally_time_speed_chart
 
 dist_speed_chart = [[0, 3, 21], [3, 5, 30], [5, 8, 40], [8, 12, 60]]
-print(Rally_time.time(dist_speed_chart))
+print(Rally_time_speed_chart.time(dist_speed_chart))

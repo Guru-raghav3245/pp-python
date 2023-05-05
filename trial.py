963e4d4c132a82198b336d13cd43b866030e024f
Guru-raghav3245 <master.guru.raghav@gmail.com>
2023-05-05 17:07:51 +0530

    trial

--- trial.py
+++ trial.py
@@ -1,7 +1,2 @@
-name = input("What is your name - ")
-while name == "":
-    if name == "":
-        name = input("Please input a valid name - ")
-    else:
-        print("done")
-        break
+num = int(input("Enter a number"))
+print(type(num))
